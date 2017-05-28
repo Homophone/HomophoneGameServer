372371949314dd1859d961053dcbfdbb50eb21e3
cloworm <chloe.echikson@gmail.com>
2017-05-28 16:48:53 -0400

    creating sample records for test

--- api/word_sets/word_sets-test.js
+++ api/word_sets/word_sets-test.js
@@ -14,8 +14,34 @@ chai.use(chaiHttp);
 
 //Our parent block
 describe('api/word_sets', () => {
-  beforeEach((done) => { //Before each test we empty the database
-    wordset.destroy({where: {}}).then(() => done());
+  beforeEach(() => { // Before each test we create some records
+    const set1 = JSON.stringify([{
+      "word": "bear",
+      "image": "https://s-media-cache-ak0.pinimg.com/736x/4f/de/00/4fde00b6e6bcbda75a35f1b234b7a38f.jpg"
+    }, {
+      "word": "bare",
+      "image": "https://thepeoplespharmacy-graedonenterpris.netdna-ssl.com/wp-content/uploads/Bare-Feet.jpg"
+    }, {
+      "word": "bair"
+    }]);
+
+    const set2 = JSON.stringify([{
+      "word": "hostel",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Hostel_Dormitory.jpg/250px-Hostel_Dormitory.jpg"
+    }, {
+      "word": "hostile",
+      "image": "https://2prowriting.files.wordpress.com/2014/12/hostility.jpg"
+    }])
+
+    return wordset.bulkCreate([{
+      words: set1
+    }, {
+      words: set2
+    }])
+  });
+
+  afterEach(() => { // After each test we empty the database
+    wordset.destroy({where: {}});
   });
 
   it('should list ALL word sets on /word_sets GET', function(done) {
@@ -25,7 +51,7 @@ describe('api/word_sets', () => {
         if (err) return done(err);
         expect(res).to.have.status(200);
         expect(res.body).to.be.an('array');
-        expect(res.body).to.have.length(0);
+        expect(res.body).to.have.length(2);
         done();
       });
   });
