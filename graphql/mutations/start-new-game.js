e8649dc0f49fe5118f39bb06076494dad25bbcbe
cloworm <chloe.echikson@gmail.com>
2017-05-29 11:26:56 -0400

    created function to return random word set

--- graphql/mutations/start-new-game.js
+++ graphql/mutations/start-new-game.js
@@ -1,17 +1,33 @@
 const game = require('../../db/models').game
 const round = require('../../db/models').round
+const wordSet = require('../../db/models').word_set
 const gameType = require('../types/game')
+
+const getRandomWordSet = () => (
+  return wordSet.find({
+    order: [
+      sequelize.fn('RANDOM')
+    ],
+    limit: 1
+  })
+)
 
 module.exports = {
   type: gameType,
   description: 'Start a new game',
   resolve(value) {
+    // create a game
     return game.create()
     .then((newGame) => {
-      return round.create({
-        gameId: newGame.id
+      // generate a random word set
+      return getRandomWordSet()
+      .then((randomWordSet) => {
+        // create first round
+        return round.create({
+          gameId: newGame.id
+        })
+        .then(() => newGame)
       })
-      .then(() => newGame)
     })
   }
 }
