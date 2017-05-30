e1c0d59421cabf3373f631f63b165cec2c793e7e
Mark Allen <mark.ivan.allen@gmail.com>
2017-05-29 21:47:58 -0400

    choose word mutation (#16)

---
+++ graphql/mutations/choose-word.js
@@ -0,0 +1,54 @@
+const {
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLString
+} = require('graphql')
+
+const round = require('../../db/models').round
+const roundType = require('../types/round')
+const ValidationError = require('../errors/validation-error')
+
+module.exports = {
+  type: roundType,
+  description: 'Start a new round for an existing game',
+  args: {
+    roundId: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'id of the round for which to choose a word'
+    },
+    word: {
+      type: GraphQLString,
+      description: 'the word chosen by the player'
+    }
+  },
+  resolve(value, { roundId, word }) {
+    return round.findById(roundId)
+    .then((roundFound) => {
+      if (roundFound.selectedAnswer) {
+        throw new ValidationError('Can\'t chooseWord on a round that already has a selectedAnswer.')
+      }
+
+      const isCorrect = word === roundFound.correctAnswer
+      const updateAttributes = {
+        isCorrect,
+        selectedAnswer: word
+      }
+
+      return Promise.all([
+        roundFound.update(updateAttributes),
+        roundFound.getGame()
+        .then((gameFound) => {
+          const livesLost = isCorrect ? 0 : 1
+          const livesRemaining = gameFound.livesRemaining - livesLost
+          const isActive = livesRemaining > 0
+
+          return gameFound.update({
+            livesRemaining,
+            isActive
+          })
+        })
+      ])
+      .then(() => roundFound)
+    })
+  }
+}
